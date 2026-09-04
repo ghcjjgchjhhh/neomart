39ba2a5029093cbb3a91d7a6150d2311c237b8d4
ghcjjgchjhhh <ifeanyiaoma2@gmail>
2026-09-05 00:54:27 +0100

    Prevent stale hosting cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neomart-shell-v2';
+const CACHE_NAME = 'neomart-shell-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
